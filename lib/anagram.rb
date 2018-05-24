a3787cb9329efded570032ac2cbc90be2be1db40
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-24 23:50:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,8 @@ class Anagram
   end
 
   def match(arr_of_matches)
-    arr_of_matches.select {|word| word.split("").sort == @sourceword.split("").sort }
+    arr_of_matches.select do |word|
+      word.split("").sort == @sourceword.split("").sort
     end
   end
 
